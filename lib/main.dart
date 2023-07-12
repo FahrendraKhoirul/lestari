264e37f446d4608c20c4e00ab2e49973fe94b275
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-07-12 15:22:14 +0800

    update to get_cli

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'views/screens/home.dart';
-import 'views/screens/splash.dart';
+
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const Landing(),
-      theme: ThemeData(
-        fontFamily: "Rubik",
-      ),
-    );
-  }
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
 }
