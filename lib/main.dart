c36cf88b95f00a259f5d26ab010c8a5a8d79157f
Fahrendra Khoirul <fahrendra.khoirul@gmail.com>
2023-07-12 15:37:30 +0800

    tes main dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Application riska",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
